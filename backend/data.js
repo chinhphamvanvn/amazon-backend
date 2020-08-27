f093c28c9c9f56ba1897a562fe4662d234e4d07f
Pham Van Chinh <chinhpv>
2020-08-27 17:44:01 +0700

    backend

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '2',
+            name: 'Baka Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 20,
+            brand: 'Azoda',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            _id: '3',
+            name: 'Best Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 10,
+            brand: 'Nike',
+            rating: 3,
+            numReviews: 5
+        },
+        {
+            _id: '4',
+            name: 'Ruby Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price:30,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 6
+        },
+        {
+            _id: '5',
+            name: 'Vinfast LUX 2.0',
+            category: 'Car',
+            image: '/images/d1.jpg',
+            price: 20,
+            brand: 'Nike',
+            rating: 2,
+            numReviews: 10
+        },
+        {
+            _id: '6',
+            name: 'Mercedes Benz 2.5',
+            category: 'Car',
+            image: '/images/d1.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 18
+        }
+    ]
+}
