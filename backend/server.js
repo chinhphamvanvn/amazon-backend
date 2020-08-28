cfe7f786f551251126b25ba8dfeb38ba5d20edd5
Pham Van Chinh <chinhpv>
2020-08-28 16:52:52 +0700

    product detail

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,14 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 })
 
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    console.log(productId);
+    const product = data.products.find(x => x._id === productId);
+    if(product)
+        res.send(product);
+    else
+        res.status(404).send({msg: "Product not found."})
+})
+
 app.listen(5000, () => { console.log("Server started at http://localhost:5000")})
