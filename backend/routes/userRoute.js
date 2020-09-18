88e72c3cbd76cda4c1e65d61cb34913a14f2cf48
chinhpv <chinhpv@icheck.vn>
2020-09-19 00:10:49 +0700

    singin part 1

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -21,4 +21,20 @@ userRoute.get("/createadmin", async (req, res) => {
     
 })
 
+userRoute.post('/signin', async(req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if(signinUser) {
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(signinUser)
+        })
+    }
+})
+
 export default userRoute;
